dbad8a1e93545ee74ae15013d97549e47a5392a7
Yusuf Bolden <ajb2316@columbia.edu>
2025-07-01 01:25:00 -0400

    Creates minimal HomePage content

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+const HomePage = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-start p-4 bg-gray-50">
+      <h1 className="text-2xl font-bold text-gray-800 my-6">
+        IP Address Tracker
+      </h1>
+    </div>
+  )
+}
+
+export default HomePage
